fa081f6c6f78a87e60b237ed69395e967d54f1de
kelmorgan <kelmorgan18@gmail.com>
2022-04-05 11:40:53 +0100

    Implemented Person Repo

--- src/main/java/com/kelmorgan/myreactiveexamples/PersonRepositoryImpl.java
+++ src/main/java/com/kelmorgan/myreactiveexamples/PersonRepositoryImpl.java
@@ -5,13 +5,20 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class PersonRepositoryImpl implements PersonRepository {
+
+    Person kufre = new Person(1,"Kufre", "Godwin");
+    Person sam = new Person(1,"Sam", "Godwin");
+    Person jesse = new Person(1,"Jesse", "Godwin");
+    Person fiona = new Person(1,"Fiona", "Godwin");
+
+
     @Override
     public Mono<Person> getById(Integer id) {
-        return null;
+        return Mono.just(kufre);
     }
 
     @Override
     public Flux<Person> findAll() {
-        return null;
+        return Flux.just(sam,kufre,jesse,fiona);
     }
 }
