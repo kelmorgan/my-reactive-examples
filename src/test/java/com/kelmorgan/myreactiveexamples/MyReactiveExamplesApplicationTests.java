1ea678343216f259937a7450211ed9aa38f6b72a
Kel Morgan <kelmorgan18@gmail.com>
2022-04-05 11:47:44 +0100

    adding tests

---
+++ src/test/java/com/kelmorgan/myreactiveexamples/MyReactiveExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelmorgan.myreactiveexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyReactiveExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
