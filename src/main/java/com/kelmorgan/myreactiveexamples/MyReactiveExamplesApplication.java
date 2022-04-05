c9dddd80102c628e41448fe26c6f3e2144c9853a
Kel Morgan <kelmorgan18@gmail.com>
2022-04-05 11:06:14 +0100

    first commit

---
+++ src/main/java/com/kelmorgan/myreactiveexamples/MyReactiveExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.kelmorgan.myreactiveexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReactiveExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReactiveExamplesApplication.class, args);
+	}
+
+}
